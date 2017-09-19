90be99dd7fb119c9447f7d347ec8e7ac613b1dd2
prabhu15bgp <prabhu15bgp@gmail.com>
2017-09-19 17:10:46 +0530

    Creating the List For Storing the data

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,28 @@
 import fresh_tomatoes
 import media
-
-toy_story=media.Movie("Toy Story",
-                      "A Story of a boy and his toys that come to life",
-                      "http://images5.fanpop.com/image/photos/30100000/Buzz-Lightyear-toy-story-2-30185761-1600-1200.jpg",
-                      "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+Title=['Toy Story','Avatar','Mersal','Interstellar','Inception','X-men','Transformer']
+StoryLine=["A Story of a boy and his toys that come to life",
+           "A Story of space sip on another Planet","A Story of Tamilan against Corprates."]
+Piclink=["http://images5.fanpop.com/image/photos/30100000/Buzz-Lightyear-toy-story-2-30185761-1600-1200.jpg",
+            "https://resizing.flixster.com/S5uksiCrVzEIio7HTEEX5Bem1rU=/206x305/v1.bTsxMTE3Njc5MjtqOzE3NDk5OzEyMDA7ODAwOzEyMDA",
+            "img/vijay.jpg"]
+Vdolink=["https://www.youtube.com/watch?v=KYz2wyBy3kc",
+         "https://www.youtube.com/watch?v=cRdxXPV9GNQ",
+         "https://www.youtube.com/watch?v=3XmrZaVVUpc"]
+toy_story=media.Movie(Title[0],
+                      StoryLine[0],
+                      Piclink[0],
+                      Vdolink[0])
 #print (toy_story.storyline)
-avatar=media.Movie("Avatar",
-                    "A Story of space sip on another Planet",
-                    "https://resizing.flixster.com/S5uksiCrVzEIio7HTEEX5Bem1rU=/206x305/v1.bTsxMTE3Njc5MjtqOzE3NDk5OzEyMDA7ODAwOzEyMDA",
-                    "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+avatar=media.Movie(Title[1],
+                    StoryLine[1],
+                    Piclink[1],
+                    Vdolink[1])
 #print (avatar.storyline)
-mersal=media.Movie("Mersal",
-                    "A Story of Tamilan",
-                    "http://img.nowrunning.com/content/movie/2017/vijay-19797/stills/Mersal-movie-02.jpg",
-                    "https://www.youtube.com/watch?v=3XmrZaVVUpc")
+mersal=media.Movie(Title[2],
+                    StoryLine[2],
+                    Piclink[2],
+                    Vdolink[2])
 #mersal.show_trailer()
 movies=[toy_story,avatar,mersal]
 fresh_tomatoes.open_movies_page(movies)
