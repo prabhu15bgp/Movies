4a9f5334e1485a431f6eaaa2f61b9a3579593238
prabhu15bgp <prabhu15bgp@gmail.com>
2017-10-04 14:12:53 +0530

    Corrections in media.py based on reviews made by udacity.

--- media.py
+++ media.py
@@ -2,15 +2,16 @@ import webbrowser
 class Movie():
     """This class provides a way to store movie related informations"""
     #google style guide for python
-    VALID_RATINGS=["G","PG","PG-13","R"]
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_video):
-        #initializes or creats space in memory
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    def __init__(self, movie_title, movie_storyline, 
+				 poster_image, trailer_video):
+        """Initialize title, storyline,poster, trailer"""
         self.title=movie_title
         self.storyline=movie_storyline
         self.poster_image_url=poster_image
         self.trailer_youtube_url=trailer_video
+		
     def show_trailer(self):
+		"""Shows the trailer in default browser. """
         webbrowser.open(self.trailer_youtube_url)
         
-        
-    
